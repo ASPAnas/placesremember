839121b6896bf2975afa0d13943440310208591e
Anastasia Popova <pananeo@yandex.ru>
2021-12-14 12:58:15 +0500

    Отобразить информацию на страницах через шаблоны

--- mysite/placesremember/urls.py
+++ mysite/placesremember/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('addnew/', noone),
+    path('', index, name='home'),
+    path('addnew/', addnew, name='addnew'),
 ]
 
--- mysite/placesremember/views.py
+++ mysite/placesremember/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'placesremember/index.html')
+    return render(request, 'placesremember/index.html', {'title': 'Храните свои впечатления о посещённых местах'})
 
-def noone(request):
-    return HttpResponse("У вас нет ни одного воспоминания")
+def addnew(request):
+    return render(request, 'placesremember/addnew.html', {'title': 'У вас нет ни одного воспоминания'})
