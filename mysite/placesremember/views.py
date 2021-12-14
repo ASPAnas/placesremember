c04dd389c8c09967126c4f523bd96dadf48aceb6
Anastasia Popova <pananeo@yandex.ru>
2021-12-14 12:41:09 +0500

    Создать набросок представления главной страницы в html-файле

--- mysite/placesremember/views.py
+++ mysite/placesremember/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Храните свои воспоминания о посещенных местах")
+    return render(request, 'placesremember/index.html')
 
 def noone(request):
     return HttpResponse("У вас нет ни одного воспоминания")
